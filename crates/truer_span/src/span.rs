d067b8b32ca8a68ab49d62d804ae00ece41d9702
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2026-08-19 07:28:08 +0300

    feat(truer_span): add Span with start and end offsets

--- crates/truer_span/src/span.rs
+++ crates/truer_span/src/span.rs
@@ -1,3 +1,23 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Span {
+    start: u32,
+    end: u32,
+}
+
+impl Span {
+    pub fn new(start: u32, end: u32) -> Self {
+        Self { start, end }
+    }
+
+    pub fn start(&self) -> u32 {
+        self.start
+    }
+
+    pub fn end(&self) -> u32 {
+        self.end
+    }
+}
+
 #[cfg(test)]
 #[path = "span_tests.rs"]
 mod tests;
